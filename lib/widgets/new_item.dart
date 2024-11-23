4543176e90cf1391c4f63b734fc352718ef8613a
Aarushi0010 <mahajanaarushi10@gmail.com>
2024-11-24 01:09:46 +0530

    validation logic added , global key logic used , reset func

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -12,85 +12,105 @@ class NewItem extends StatefulWidget{
 }
 
 class _NewItemState extends State<NewItem>{
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem(){
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(context){
     return Scaffold (
       appBar: AppBar(
         title : const Text('Add a new item'),
       ),
-      body:Padding(
-        padding: const EdgeInsets.all(12),
-        child: Form(child: Column(
-          children: [
-            TextFormField(
-              maxLength: 5 ,
-              decoration: const InputDecoration(
-                label: Text('Name'),
-              ) ,
-              validator : (value){
-                if (value == null ||
-                      value.isEmpty ||
-                      value.trim().length <= 1 ||
-                      value.trim().length > 50) {
-                    return 'Must have 1 to 50 characters';
-                  }
-                return null;
-              },
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: TextFormField(
+        body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    maxLength: 5,
                     decoration: const InputDecoration(
-                      label: Text('Quantity'),
+                      label: Text('Name'),
                     ),
-                    initialValue: '1',
-                    validator : (value){
+                    validator: (value) {
                       if (value == null ||
                           value.isEmpty ||
-                          int.tryParse(value) == null ||
-                          int.tryParse(value)! <= 0 ) {
-                        return 'Must be a valid positive number';
+                          value.trim().length <= 1 ||
+                          value.trim().length > 50) {
+                        return 'Must have 1 to 50 characters';
                       }
                       return null;
                     },
                   ),
-
-                ),
-                const SizedBox(width: 8,),
-                Expanded(
-                  child: DropdownButtonFormField(
-                      items: [for (final category in categories.entries)
-                        DropdownMenuItem(
-                          value: category.value,
-                            child: Row(
-                            children: [
-                              Container(
-                                width: 16,
-                                height: 16,
-                                color : category.value.color,
-                              ),
-                              const SizedBox(width: 8,),
-                              Text(category.value.title),
-                            ],
-                        ))
-                        ],
-                      onChanged: (value){}, ),
-                )
-              ],
-            ),
-            const SizedBox(height: 12,),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                TextButton(onPressed: (){}, child: const Text('Reset')),
-                ElevatedButton(onPressed: (){}, child: const Text('Add Item'))
-              ],
-            )
-          ],
-        )),
-      )
-    );
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text('Quantity'),
+                          ),
+                          keyboardType: TextInputType.number,
+                          initialValue: '1',
+                          validator: (value) {
+                            if (value == null ||
+                                value.isEmpty ||
+                                int.tryParse(value) == null ||
+                                int.tryParse(value)! <= 0) {
+                              return 'Must be a valid positive number';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Expanded(
+                        child: DropdownButtonFormField(
+                          items: [
+                            for (final category in categories.entries)
+                              DropdownMenuItem(
+                                  value: category.value,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        width: 16,
+                                        height: 16,
+                                        color: category.value.color,
+                                      ),
+                                      const SizedBox(
+                                        width: 8,
+                                      ),
+                                      Text(category.value.title),
+                                    ],
+                                  ))
+                          ],
+                          onChanged: (value) {},
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      TextButton(
+                          onPressed: () {
+                            _formKey.currentState!.reset();
+                          },
+                          child: const Text('Reset')),
+                      ElevatedButton(
+                          onPressed: _saveItem, child: const Text('Add Item'))
+                    ],
+                  ),
+                ],
+              )),
+        ));
   }
 }
