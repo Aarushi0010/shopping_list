66205bd85959499dac6262c2679dad26f7c3fa08
Aarushi0010 <mahajanaarushi10@gmail.com>
2024-11-24 23:57:37 +0530

    http.post successful

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -4,6 +4,8 @@ import 'package:flutter/widgets.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class NewItem extends StatefulWidget{
   const NewItem({super.key}) ;
 
@@ -22,6 +24,19 @@ class _NewItemState extends State<NewItem>{
   void _saveItem(){
     if(_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      final url = Uri.http('shopping-list-a6cb3-default-rtdb.firebaseio.com','shopping-list.json' );
+      http.post(url,
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          }
+        )
+      );
       Navigator.of(context).pop(GroceryItem(
           id: DateTime.now().toString(),
           name: _enteredName,
